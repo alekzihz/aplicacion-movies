6d09dd3c936057094ba2f9a2c494474d2faec0e9
Alexis <57765568+alekzihz@users.noreply.github.com>
2023-11-26 21:43:37 +0100

    datos parsed stringJson to Objectjson

--- model/mongoDB/movie.js
+++ model/mongoDB/movie.js
@@ -12,8 +12,8 @@ async function connect() {
   try {
     await client.connect();
     //console.log('Conexión exitosa a la base de datos.');
-    const database = client.db('appmovie');
-    return database.collection('moviedb');
+    const database = client.db(process.env.DB_NAME);
+    return database.collection(process.env.DB_COLLECTION);
     
   } catch (error) {
     console.error('Error al conectar a la base de datos:', error);
@@ -24,27 +24,25 @@ async function connect() {
 export class MovieModel{
     static async getAllMovies(){
         const movies = await connect();
-        const lista= await movies.find({}).limit(100).toArray();
+        const lista= await movies.find({}).limit(10).toArray();
 
-        const moviesWithParsedGenres = lista.map(movie => {
+        const moviesParsed = lista.map(movie => {
           const fixedGenres = typeof movie.genres === 'string' ? movie.genres.replace(/'/g, '"') : null;
-          
-
-          //const fixedCollection = movie.belongs_to_collection === 'string' ? movies.belongs_to_collection.replace(/'/g, '"') : null;
-          
-          //console.log(fixedGenres)
+          const fixedProduction_Countries = typeof movie.production_countries === 'string' ? movie.production_countries.replace(/'/g, '"') : null;
+          const fixedCollection = movie.belongs_to_collection ? movie.belongs_to_collection.replace(/'/g, '"') : null;
+          const fixedProduction = movie.production_companies ? movie.production_companies.replace(/'/g, '"') : null;
+          const fixedLanguages = movie.spoken_languages ? movie.spoken_languages.replace(/'/g, '"') : null;
           return {
               ...movie,
               genres: fixedGenres? JSON.parse(fixedGenres): null,
-              //belongs_to_collection: fixedCollection? JSON.parse(fixedCollection): null
-              //belongs_to_collection: JSON.parse(fixedCollection)
-              // Agrega otras conversiones según sea necesario
+              belongs_to_collection: fixedCollection? JSON.parse(fixedCollection): null,
+              production_countries: fixedProduction_Countries? JSON.parse(fixedProduction_Countries): null,
+              production_companies: fixedProduction? JSON.parse(fixedProduction): null,
+              spoken_languages: fixedLanguages? JSON.parse(fixedLanguages): null
           };
       });
   
-      console.log(moviesWithParsedGenres);
-
-      return moviesWithParsedGenres;
+      return moviesParsed;
         
         //console.log(lista)
         //const formattedMovies= lista.map((movie) => JSON.stringify(movie));
