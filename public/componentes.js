93cbd4aa419f842ff27e9eb39e2ca782e06ab420
Alexis <57765568+alekzihz@users.noreply.github.com>
2023-12-04 05:10:52 +0100

    implementacion de barra de busqueda

--- public/componentes.js
+++ public/componentes.js
@@ -3,6 +3,8 @@ const RootComponent = {
     data() {
         return {
             movie: "",
+            movieFiltradas:"",
+            cacheMovies: [],
             message: "APP Movies!",
             loading: true
         };
@@ -19,24 +21,37 @@ const RootComponent = {
                 const data = await response.json();
                 this.movie = data;
                 this.loading = false;
-                console.log("completado");
+                this.cacheMovies = data;
             } catch (error) {
                 console.error("Error al obtener películas:", error);
                 // Manejar el error de alguna manera, por ejemplo, establecer this.loading en false
                 this.loading = false;
             }
-        }
+        },
+        controladorBusqueda(moviesFiltradas){
+            console.log("controladorBusqueda")
+            console.log(moviesFiltradas)
+            if(moviesFiltradas.length == 0){
+                console.log("esto es cero")
+                this.movie = this.cacheMovies;
+            }
+            else{
+                this.movie = moviesFiltradas;
+            }
+        },
     },
 
     template:`
     
     <div>
-        <h1>{{ message }}</h1>
+        <!--h1>{{ message }}</h1-->
         <!-- Mostrar un indicador de carga mientras se obtienen las películas -->
         <div v-if="loading">Cargando...</div>
+
+        <FilterMovie :peliculas="movie" @busqueda="controladorBusqueda"></FilterMovie> 
         <!-- Mostrar la lista de películas cuando la carga ha terminado -->
-        <div v-if="!loading">
-            <MovieItem v-for="movie in movie" :key="movie.id" :movie="movie" />
+        <div v-if="!loading" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
+        <MovieItem v-for="movie in movie" :key="movie.id" :movie="movie" />
         </div>
     </div>
     `
@@ -59,8 +74,7 @@ const MovieItem = {
             //const tmdbUrl = 'https://image.tmdb.org/t/p/w500' + path_image;
             //const filmtoroUrl = 'https://filmtoro.cz/img/film' + path_image;
             try {
-                const resposta = await fetch('movies/image' + path_image);
-               
+                const resposta = await fetch('movies/image' + path_image);               
                 const blob = await resposta.blob();
 
         // Crear una URL de datos directamente desde el Blob
@@ -74,40 +88,65 @@ const MovieItem = {
 
 
     mounted() {
-        
-
-        this.getImage();
-        //console.log(this.movie.belongs_to_collection)
-        //console.log(this.movie)        
-        /*if ('belongs_to_collection' in this.movie){
-            this.belongs_to_collection = JSON.parse(this.movie.belongs_to_collection.replace(/'/g, '"'))
-            this.poster_path = 'https://image.tmdb.org/t/p/w500'+this.belongs_to_collection.poster_path;        
-        }
-        
-        else{
-            this.poster_path = 'https://image.tmdb.org/t/p/w500'+this.movie.poster_path;
-        }*/
-
-        //this.poster_path = 'https://filmtoro.cz/img/film'+this.movie.poster_path;
+        this.getImage();      
     },
     template: `
-    <div>
-        <img :src="poster_path" :alt="movie.title" />
-        <h2>{{ movie.title }}</h2>
-        <p>{{ movie.overview }}</p>
-    </div>
+    <section class="max-w-md mx-auto bg-white rounded-xl overflow-hidden shadow-md md:w-48 md:h-64 lg:w-64 lg:h-80">
+        <img class="w-full h-48 object-cover" :src="poster_path" alt="movie.title">
+        <div class="p-6">
+            <h2 class="font-bold text-xl mb-2">{{ movie.title }}</h2>
+            <p class="text-gray-700">{{ movie.description }}</p>
+        </div>
+    </section>
+
+
+
+
     `
+}
 
-    //<img :src="'https://image.tmdb.org/t/p/w500'+poster_path" :alt="movie.title" />
+const FilterMovie = {
 
-    //<img :src="https://image.tmdb.org/t/p/w500'+poster_path" :alt="movie.title" />
+    emits:["busqueda"],
+    props:["peliculas"],
 
+    data(){
+        return{
+            searchQuery: "",
+            peliculasEncontradas: []
+        }
+
+    },
+    methods:{
+        searchMovies(){
+
+            if(this.searchQuery == ""){
+                this.peliculasEncontradas = [];
+                this.$emit("busqueda", this.peliculasEncontradas);
+                return;
+            }
+            this.peliculasEncontradas = this.peliculas.filter((movie) => {
+                return movie.title.toLowerCase().includes(this.searchQuery.toLowerCase());
+            });
+        
+            //console.log(this.peliculasEncontradas)
+            this.$emit("busqueda", this.peliculasEncontradas);
+        }
+
+    },
+
+    template:`
+    <input v-model="searchQuery" @input="searchMovies" placeholder="Buscar películas" class="p-2 mb-4 border-gray-900 w-full">
+
+    `
+    
+    
 }
 
 
 
 
 const app = Vue.createApp(RootComponent);
-
 app.component("MovieItem",MovieItem);
+app.component("FilterMovie", FilterMovie);
 const vm = app.mount("#app");
